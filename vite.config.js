d192d3886082bb60ee2b9f106ed4d507a8e7766c
AnanyaChaba <ananyachaba11@gmail.com>
2026-05-25 22:41:44 +0530

    na

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "economist-chart",
+  base: "/economist-chart/",
   plugins: [react()],
 });
